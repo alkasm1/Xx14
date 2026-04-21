c9795d7766fc45c3bc9c9d1d49fedde314532593
رأفت عمر <shamwebs1@gmail.com>
2026-04-21 13:18:48 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = "xx16-alm-v1";
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./script.js",
+  "./style.css",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
